a892aacd0d33dd349f2861293578796536eea57e
Toshito Kimura <a4392966@gmail.com>
2024-01-12 00:44:48 +0900

    Fix test

--- test.bash
+++ test.bash
@@ -11,7 +11,7 @@ ng () {
 res=0
 
 ### I/O テスト ###
-out=$(python3 trig_calculator.py s 30)
+out=$(./trig_calculator s 30)
 [ "${out}" = "sin 30.0(deg) = 0.500" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
